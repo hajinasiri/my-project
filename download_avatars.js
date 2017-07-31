5705eeaa063c6ce1ecd47fee42cd1dabd66794b1
hajinasiri <hajinasiri@gmail.com>
2017-07-31 21:18:40 +0000

    defining the function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,19 @@
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+  request.get("https://github.com/nodejs/node")               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         cb(err,null);
+       })
+       .on('response', function (response) {                           // Note 3
+        cb(null,response);
+       })
+}
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
