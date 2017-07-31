7605ebf980dfe205ab4a72adf8fbfe3c6d5a192c
hajinasiri <hajinasiri@gmail.com>
2017-07-31 21:32:21 +0000

    adding the url creator part

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,6 @@
+var GITHUB_USER = "hajinasiri";
+var GITHUB_TOKEN = "35e9aae1b4a51c485662c41165537e7cc6077e5e";
+
 var request = require('request');
 var fs = require('fs');
 
@@ -5,12 +8,14 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log(requestURL);
   request.get("https://github.com/nodejs/node")               // Note 1
        .on('error', function (err) {                                   // Note 2
-         cb(err,null);
+         // cb(err,null);
        })
        .on('response', function (response) {                           // Note 3
-        cb(null,response);
+        // cb(null,response);
        })
 }
 getRepoContributors("jquery", "jquery", function(err, result) {
