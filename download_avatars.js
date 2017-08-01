48234cef7eedcf50b53252ab6c324563591fb8e0
hajinasiri <hajinasiri@gmail.com>
2017-08-01 00:16:32 +0000

    adding step 7

--- download_avatars.js
+++ download_avatars.js
@@ -27,3 +27,21 @@ getRepoContributors("jquery", "jquery", function(err, result) {
     console.log(item.avatar_url);
   });
 });
+
+
+
+function downloadImageByURL(url, filePath) {
+  // ...
+  console.log("downloading image")
+request.get(url)               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream(filePath));
+       console.log("Download completed!");
+
+};
+downloadImageByURL("https://avatars0.githubusercontent.com/u/28185?v=4", "./kvirani.jpg");
