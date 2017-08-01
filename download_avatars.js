14c6a6921ff1aee99347ad3029d34b849c7852be
hajinasiri <hajinasiri@gmail.com>
2017-08-01 01:29:43 +0000

    final

--- download_avatars.js
+++ download_avatars.js
@@ -5,49 +5,47 @@ var request = require('request');
 var fs = require('fs');
 var owner = process.argv[2];
 var repo = process.argv[3];
-if(repo && owner){
-  console.log('Welcome to the GitHub Avatar Downloader!');
-  function getRepoContributors(repoOwner, repoName, cb) {
-    // ...
+//for example owner = repo = jquery
 
-    var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-    var options = {
-      url: requestURL,
-      headers: {
-      'User-Agent': "GitHub Avatar Downloader - Student Project"
-    }
-  };
-    request.get(options,function(err, response, body){
-      // console.log(JSON.parse(body));
-      // console.log(err,body);
-      return cb(JSON.parse(body));
+console.log('Welcome to the GitHub Avatar Downloader!');
+function getRepoContributors(repoOwner, repoName, cb) {
+  // cb is the callback function
+  //creating request URL
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
-    });
+  //creating option object to get rid of the forbidden access error
+  var options = {
+    url: requestURL,
+    headers: {
+        'User-Agent': "GitHub Avatar Downloader - Student Project"
   }
-  //owner = repo = jquery
+};
+  request.get(options,function(err, response, body){
+    //returning the body of the response in JSON format
+    return cb(JSON.parse(body));
 
+  });
+}
+function downloadImageByURL(body) {
+  // ...
+
+  body.forEach(function(item){
+    var filePath = "./avatars/" + item.login + ".jpg";
+    var url = item.avatar_url;
+    request.get(url)               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+       })
+       .pipe(fs.createWriteStream(filePath));
+       console.log(item.login + "'s photo download completed!");
+  });
+}
+
+if(owner && repo){
   getRepoContributors(owner, repo,downloadImageByURL);
-
-
-
-  function downloadImageByURL(body) {
-    // ...
-
-    body.forEach(function(item){
-      var filePath = "./avatars/" + item.login + ".jpg";
-      var url = item.avatar_url;
-      request.get(url)               // Note 1
-         .on('error', function (err) {                                   // Note 2
-           throw err;
-         })
-         .on('response', function (response) {                           // Note 3
-         })
-         .pipe(fs.createWriteStream(filePath));
-         console.log(item.login + "'s photo download completed!");
-    });
-  }
 }else{
   console.log("command should be in the format of node download_avatars.js <owner> <repo>")
 }
-
 
