cea16fe3cc57068501d02d0d51f033133b60307e
hajinasiri <hajinasiri@gmail.com>
2017-08-01 00:55:16 +0000

    step 8 got added

--- download_avatars.js
+++ download_avatars.js
@@ -8,40 +8,42 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-var options = {
-  url: requestURL,
-  headers: {
+  console.log(requestURL);
+  var options = {
+    url: requestURL,
+    headers: {
     'User-Agent': "GitHub Avatar Downloader - Student Project"
   }
 };
-  console.log(requestURL)
   request.get(options,function(err, response, body){
-    return cb(err, JSON.parse(body));
+    // console.log(JSON.parse(body));
+    // console.log(err,body);
+    return cb(JSON.parse(body));
+
   });
 }
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  result.forEach(function(item){
-    console.log(item.avatar_url);
-  });
-});
+getRepoContributors("jquery", "jquery",downloadImageByURL);
 
 
 
-function downloadImageByURL(url, filePath) {
+function downloadImageByURL(body) {
   // ...
-  console.log("downloading image")
-request.get(url)               // Note 1
+
+  body.forEach(function(item){
+    var filePath = "./avatars/" + item.login + ".jpg";
+    var url = item.avatar_url;
+    request.get(url)               // Note 1
        .on('error', function (err) {                                   // Note 2
          throw err;
        })
        .on('response', function (response) {                           // Note 3
-         console.log('Response Status Code: ', response.statusCode);
        })
        .pipe(fs.createWriteStream(filePath));
-       console.log("Download completed!");
+       console.log(item.login + "'s photo download completed!");
+  });
+}
 
-};
-downloadImageByURL("https://avatars0.githubusercontent.com/u/28185?v=4", "./kvirani.jpg");
+
+
